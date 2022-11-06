9aa19210c205f09234e22f1f45cfe75a084c43e6
Kevin Willian <kevinwillian084@gmail.com>
2022-11-06 18:38:35 -0300

    feat: adiciona base do projeto web

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import { Center } from 'native-base';
+import Logo from '../assets/logo.svg';
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Logo width={212} height={40} />
+    </Center>
+  );
+}
