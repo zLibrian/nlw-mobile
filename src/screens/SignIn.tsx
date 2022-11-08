08001eaf662884a7c7692a5fb687710035f19396
Kevin Willian <kevinwillian084@gmail.com>
2022-11-07 23:59:30 -0300

    teest

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -5,6 +5,7 @@ export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
       <Logo width={212} height={40} />
+      <Logo width={212} height={40} />
     </Center>
   );
 }
